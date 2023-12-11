aab4f06ad9f6646c2f8c2e5e1edd0d3d0d9e16b2
Nikhil Shahare <nikshahare1998@gmail.com>
2023-12-11 22:13:26 +0530

    edited preview

--- frontend/src/Pages/Preview.jsx
+++ frontend/src/Pages/Preview.jsx
@@ -3,18 +3,21 @@ import { useNavigate } from 'react-router-dom'
 
 const Preview = () => {
 const blogContent = JSON.parse(localStorage.getItem("blog"))
+const title = JSON.parse(localStorage.getItem("title"))
 const navigate =useNavigate()
 
 const handleClick =()=>navigate("/")
   return (
-    <div className='mx-auto bg-red-100  '>
-        <h1 className='text-4xl'>Preview</h1>
-        <div className='flex justify-between'>
-           <button onClick={handleClick} className='rounded border-red-200 ' >back</button>
+    <div className='mx-auto bg-white '>
+        <div className='flex justify-between py-10'>
+        <h1 className='text-3xl font-semibold' >{title}</h1>
         </div>
+        <div class="border-t border-gray-100 my-10 mx-2"></div>
+        <div class="border-t border-gray-100 my-10 mx-2"></div>
 
         <div className='blog bg-white max-w-3xl p-2 mx-2' dangerouslySetInnerHTML={{ __html: blogContent }}/>
 
+        <button onClick={handleClick} className='rounded border-red-200  ' >back</button>
     </div>
   )
 }
--- frontend/src/components/Editor/Editor.jsx
+++ frontend/src/components/Editor/Editor.jsx
@@ -1,80 +1,89 @@
-import React,{useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 import { Editor } from '@tinymce/tinymce-react';
 import "./Editor.css"
 import { useNavigate } from 'react-router-dom';
 
 const EditorTab = () => {
     const navigate = useNavigate()
-
-    const [modal,setModal] = useState(false)
-    const [storedContent,setStoredContent] =useState("")
-const initialVal = localStorage.getItem("blog")?JSON.parse(localStorage.getItem("blog")):"hare krishna"
- console.log("I am initial val",initialVal)
-
- const handlepreview =async()=>{
-     handleSave()
-     navigate("/preview")
-     
+    if(!localStorage.getItem("title")){
+        localStorage.setItem("title", JSON.stringify(false))
     }
+     
+    const title =  localStorage.getItem("title") ? JSON.parse(localStorage.getItem("title")) : "hare krishna"
+    const [storage, setStorage] = useState(title)
+
+    const initialVal = localStorage.getItem("blog") ? JSON.parse(localStorage.getItem("blog")) : "hare krishna"
+
+    const handlepreview = async () => {
+        handleSave()
+        navigate("/preview")
+
+    }
+
+    const handleSave = () => {
+        localStorage.setItem("blog", JSON.stringify(editorRef.current.getContent()))
+        localStorage.setItem("title", JSON.stringify(storage))
+    }
+
     
-const handleSave =()=>{
-    localStorage.setItem("blog",JSON.stringify(editorRef.current.getContent()))
-}
-        
-        
-        const handleModal =()=>setModal(prev=>!prev)
-        
-        const editorRef = useRef(null);
-        
-    console.log(editorRef.current)
-   const log = () => {
-     if (editorRef.current) {
-       console.log(editorRef.current.getContent());
-     }
-   };
+    const handleChange = (e) => setStorage(e.target.value)
 
-  return (
-    <div  className = "editor-container max-w-5xl    "
-    >
-        <div className='title flex flex-col items-center my-1 font-mono '>
-     <h1 className='flex w-24 text-4xl mb-2'>Title</h1>
-        </div>
-     <input type='text' className='title-input  rounded h-10 mb-10 shadow-md'></input>
-        <div className='shadow-md'>
+    const editorRef = useRef(null);
 
-    <Editor
-         onInit={(evt, editor) => editorRef.current = editor}
-         apiKey='orq5ev0twhr6uu62jnse4t95fbe7ng9cjc127q94d2jqxlvz'
-         init={{
-             selector:"textarea",
-             height:900,
-             width:768,
-             plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
-             toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-             tinycomments_mode: 'embedded',
-             tinycomments_author: 'Author name',
-             images_resizing: true,
-             image_advtab: true,
-             mergetags_list: [
-                 { value: 'First.Name', title: 'First Name' },
-                 { value: 'Email', title: 'Email' },
-                ],
-                ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-            }}
-            initialValue={initialVal}
-            />
+    console.log("i am storage",storage)
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent());
+        }
+    };
+
+    return (
+        <div className="editor-container max-w-5xl    "
+        >
+            <div className='title flex flex-col items-center my-1 font-mono '>
+                <h1 className='flex w-24 text-4xl mb-2'>Title</h1>
             </div>
-      <div className='flex justify-center mt-5'>
-      <button className='bg-green-500 rounded-md px-2 w-48 mx-2 my-2 text-3xl' onClick={handlepreview}>Preview</button>  
-      <button className='bg-green-500 rounded-md px-2 w-48 mx-2 my-2 text-3xl' onClick={handleSave}>save</button>
-      
-        <button className='bg-green-500 rounded-md px-2 w-48 mx-2 my-2 text-3xl' onClick={log}>Publish</button>
-      </div>
-      
+            <input
+                type='text'
+                className='title-input  rounded h-10 mb-10 shadow-md'
+                value={storage}
+                onChange={handleChange}
+            ></input>
+            <div className='shadow-md'>
+
+                <Editor
+                    onInit={(evt, editor) => editorRef.current = editor}
+                    apiKey='orq5ev0twhr6uu62jnse4t95fbe7ng9cjc127q94d2jqxlvz'
+                    init={{
+                        selector: "textarea",
+                        height: 900,
+                        width: 768,
+                        plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+                        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                        tinycomments_mode: 'embedded',
+                        tinycomments_author: 'Author name',
+                        images_resizing: true,
+                        image_advtab: true,
+                        mergetags_list: [
+                            { value: 'First.Name', title: 'First Name' },
+                            { value: 'Email', title: 'Email' },
+                        ],
+                        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+                    }}
+                    initialValue={initialVal}
+                />
+            </div>
+            <div className='flex justify-center mt-5'>
+                <button className='bg-green-500 rounded-md px-2 w-48 mx-2 my-2 text-3xl' onClick={handlepreview}>Preview</button>
+                <button className='bg-green-500 rounded-md px-2 w-48 mx-2 my-2 text-3xl' onClick={handleSave}>save</button>
+
+                <button className='bg-green-500 rounded-md px-2 w-48 mx-2 my-2 text-3xl' onClick={log}>Publish</button>
+            </div>
 
 
-      </div>
-  )
+
+        </div>
+    )
 }
 
 export default EditorTab
